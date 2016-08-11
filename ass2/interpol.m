1994030f5368b4ecd2ea476e30abf6974e3acc38
anirudt <anirudt@gmail.com>
2016-08-11 16:51:36 +0530

    started interpol, done with NNinterpol in matlab

---
+++ ass2/interpol.m
@@ -0,0 +1,20 @@
+% Interpolation techniques in MATLAB
+function y = interpol(img)
+% Main function for testing out different kinds of interpolation techniques
+img = imread('cameraman.tif');
+y = NNinterpol(img, 512, 512);
+imshow(y);
+end
+
+function interpolated = NNinterpol(img, M, N)
+  [R, C] = size(img);
+  j = floor(M/R);
+  k = floor(N/C);
+  repeat = ones(j, k);
+  interpolated = kron(im2double(img), repeat);
+end
+
+
+function interpolated = BLinterpol(img, M, N)
+
+end
